197186fcb586fdeca7fb1ab569b94dd5cf8b8343
Ararat Tonoyan <tonoyandeveloper@gmail.com>
2020-10-03 17:29:43 +0300

    Swap app Init commit

---
+++ swap/assets/js/rootui-init.js
@@ -0,0 +1,10 @@
+/*!
+ * Name: RootUI - Dashboards and Administration areas for React + Redux and HTML
+ * Version: 1.1.4
+ * Author: dexad, nK
+ * Website: https://nkdev.info/
+ * Purchase: https://themeforest.net/user/_nk/portfolio
+ * Support: https://nk.ticksy.com/
+ * License: You must have a valid license purchased only from ThemeForest (the above link) in order to legally use the theme for your project.
+ * Copyright 2020.
+ */!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=1)}([function(e,t,n){"use strict";n.d(t,"a",(function(){return r}));var r={ajax:!0,events:{onBeforeAjax:function(){return!1},onAfterAjax:function(){}}}},function(e,t,n){"use strict";n.r(t);var r=n(0);void 0!==window.RootUI&&(window.RootUI.setOptions(r.a),window.RootUI.init())}]);
---
+++ swap/assets/js/script.js
@@ -0,0 +1,140 @@
+// address needs updating to mainnet
+var PurchasePortal = "";
+
+var sbreeToken = "";
+
+var contract_abi = [{ "inputs": [{ "internalType": "address", "name": "breeTokenAddress", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "_purchaser", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "_tokens", "type": "uint256" }], "name": "TOKENSPURCHASED", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "assetAddress", "type": "address" }, { "internalType": "uint256", "name": "amountAsset", "type": "uint256" }], "name": "purchase", "outputs": [], "stateMutability": "payable", "type": "function" }];
+
+var sbree_abi = [{ "inputs": [{ "internalType": "string", "name": "name", "type": "string" }, { "internalType": "string", "name": "symbol", "type": "string" }, { "internalType": "uint8", "name": "decimals", "type": "uint8" }, { "internalType": "uint256", "name": "cap", "type": "uint256" }, { "internalType": "uint256", "name": "initialSupply", "type": "uint256" }, { "internalType": "bool", "name": "transferEnabled", "type": "bool" }, { "internalType": "bool", "name": "mintingFinished", "type": "bool" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [], "name": "MintFinished", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }], "name": "RoleGranted", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }], "name": "RoleRevoked", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [], "name": "TransferEnabled", "type": "event" }, { "inputs": [], "name": "BUILT_ON", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "DEFAULT_ADMIN_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "MINTER_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "OPERATOR_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "approveAndCall", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "name": "approveAndCall", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "burn", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "burnFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "cap", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" }], "name": "decreaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "enableTransfer", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "finishMinting", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }], "name": "getRoleAdmin", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "uint256", "name": "index", "type": "uint256" }], "name": "getRoleMember", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }], "name": "getRoleMemberCount", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "grantRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "hasRole", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" }], "name": "increaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "mintingFinished", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "tokenAddress", "type": "address" }, { "internalType": "uint256", "name": "tokenAmount", "type": "uint256" }], "name": "recoverERC20", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "renounceRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "revokeRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" }], "name": "supportsInterface", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "transferAndCall", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "name": "transferAndCall", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "transferEnabled", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "name": "transferFromAndCall", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "transferFromAndCall", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
+
+var accounts, SBREE_CONTRACT, PURCHASE_CONTRACT;
+
+async function metamaskIntegration() {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {} else {
+        alert("The File APIs are not fully supported in this browser.");
+    }
+
+    //Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        try {
+            //Request account access
+            //if needed
+            metaMask = false;
+            ethereum.enable().then(metaMask = true);
+        } catch (error) {
+            metaMask = false;
+            alert('Connect to metamask');
+            //User denied account access...
+        }
+    }
+    //Legacy dapp browsers...
+    else if (window.web3) {
+        metaMask = true;
+        window.web3 = new Web3(web3.currentProvider);
+    }
+    //Non - dapp browsers...
+    else {
+        metaMask = false;
+        alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+
+    if (metaMask) {
+        $('#account').removeClass('d-none');
+    } else {
+
+    }
+
+    window.ethereum.on('accountsChanged', function(_accounts) {
+        // Time to reload your interface with accounts[0]!
+        // window.location.reload(true);
+    });
+
+    await web3.eth.getAccounts(function(error, result) {
+        if (!error) {
+            accounts = result;
+        } else
+            console.error(error);
+    });
+}
+
+async function contractInitialization(contractAddress, contractABI) {
+    return (await new web3.eth.Contract(contractABI, contractAddress));
+}
+
+function convert(n) {
+    var sign = +n < 0 ? "-" : "",
+        toStr = n.toString();
+    if (!/e/i.test(toStr)) {
+        return n;
+    }
+    var [lead, decimal, pow] = n.toString()
+        .replace(/^-/, "")
+        .replace(/^([0-9]+)(e.*)/, "$1.$2")
+        .split(/e|\./);
+    return +pow < 0 ?
+        sign + "0." + "0".repeat(Math.max(Math.abs(pow) - 1 || 0, 0)) + lead + decimal :
+        sign + lead + (+pow >= decimal.length ? (decimal + "0".repeat(Math.max(+pow - decimal.length || 0, 0))) : (decimal.slice(0, +pow) + "." + decimal.slice(+pow)))
+}
+
+$(function() {
+
+    metamaskIntegration().then(x => {
+
+        $('#account').html(accounts[0].substring(0, 6) + '...' + accounts[0].substring(36, 41));
+
+        contractInitialization(sbreeToken, sbree_abi).then(sbree => {
+            SBREE_CONTRACT = sbree;
+        });
+
+        contractInitialization(PurchasePortal, contract_abi).then(c => {
+            PURCHASE_CONTRACT = c;
+        });
+
+    });
+
+
+    $('#connectWallet').click(function(e) {
+        e.preventDefault();
+        metamaskIntegration().then(x => {
+            $('#account').html(accounts[0].substring(0, 6) + '...' + accounts[0].substring(36, 41));
+        });
+    });
+
+    $('#token_amount_all').click(async function(e) {
+        e.preventDefault();
+        var totalBalance = await SBREE_CONTRACT.methods.balanceOf(accounts[0]).call();
+        totalBalance = totalBalance / 10 ** (18);
+        $('#token_amount').val(totalBalance);
+        $('#token_swap').val(totalBalance);
+    });
+
+    $('#approveTokens').click(function(e) {
+        e.preventDefault();
+        var tokens = $('#token_amount').val();
+        tokens = (convert(tokens * 10 ** (18))).toString();
+        SBREE_CONTRACT.methods.approve(PurchasePortal, tokens).send({
+            from: accounts[0],
+            gasLimit: 210000
+        }).on('transactionHash', function(hash) {
+            $('#status').html('Approve Transaction is pending');
+        }).on('confirmation', function(confirmationNumber, receipt) {
+            $('#status').html('Tokens have been approved and it is ready to be swapped.');
+        });
+    });
+
+    $('#swapBtn').click(function(e) {
+        e.preventDefault();
+        var tokens = $('#token_amount').val();
+        tokens = (convert(tokens * 10 ** (18))).toString()
+        PURCHASE_CONTRACT.methods.purchase(sbreeToken, tokens).send({
+            from: accounts[0],
+            gasLimit: 210000
+        }).on('transactionHash', function(hash) {
+            $('#status').html('Swap Transaction is pending');
+        }).on('confirmation', function(confirmationNumber, receipt) {
+            $('#status').html('Tokens have been swapped, please check your wallet.');
+        });
+    });
+
+});
