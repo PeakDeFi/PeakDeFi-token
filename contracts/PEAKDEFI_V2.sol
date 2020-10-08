50d66a83992f0aea8ff29db1480d11cccd7a3d00
Ararat Tonoyan <tonoyandeveloper@gmail.com>
2020-10-08 15:05:41 +0300

    Added comment for non-usable contact

--- contracts/PEAKDEFI_V2.sol
+++ contracts/PEAKDEFI_V2.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.2;
 
 import "./PEAKDEFI_V1.sol";
 
-
+// MOCK TOKEN FOR UNIT TESTING
 contract PEAKDEFI_V2 is PEAKDEFI_V1 {
     function changeTokenData(string calldata newName, string calldata symbol, uint8 newDecimal) external {
         _initialize(newName, symbol, newDecimal);
